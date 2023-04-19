daba998bd4a31ec8ee5ccde281a45f914f65b2a4
RahulRawatAgt <rahulrawat.agt@gmail.com>
2023-04-19 15:48:53 +0530

    included the dbconnection

--- inquiryconnect.php
+++ inquiryconnect.php
@@ -1,16 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "retinadb";
+include ('dbconnection.php');  
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+
 
 // Verify the reCAPTCHA response
 $recaptcha_response = $_POST['g-recaptcha-response'];
